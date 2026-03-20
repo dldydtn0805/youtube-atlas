d81a8086e55eaaaeee2c767a80c0fc039a15b213
dldydtn0805 <139421083+dldydtn0805@users.noreply.github.com>
2026-03-20 15:46:56 +0900

    Enable autoplay for selected videos

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -8,14 +8,19 @@ interface VideoPlayerProps {
 
 function VideoPlayer({ selectedVideoId }: VideoPlayerProps) {
   const videoId = selectedVideoId ?? DEFAULT_VIDEO_ID;
+  const embedParams = new URLSearchParams({
+    autoplay: '1',
+    rel: '0',
+  });
 
   return (
     <section className="video-player">
       <div className="video-player__frame">
         <iframe
+          allow="autoplay; encrypted-media; picture-in-picture"
           allowFullScreen
           key={videoId}
-          src={`https://www.youtube.com/embed/${videoId}`}
+          src={`https://www.youtube.com/embed/${videoId}?${embedParams.toString()}`}
           title="Selected YouTube video"
         />
       </div>
